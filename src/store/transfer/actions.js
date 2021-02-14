00ded43686b7f1588cb8d89868604f4d59dc1b04
wicky <wicky@prideandjoy.org>
2021-02-14 20:52:12 +1300

    tidy up code, update response

--- src/store/transfer/actions.js
+++ src/store/transfer/actions.js
@@ -1,4 +1,25 @@
 export const sendTokens = async function ({ state }, { memo, quantity, to }) {
+  const config = [{
+    actions: [{
+      account: process.env.TOKEN_SMARTCONTRACT,
+      name: 'transfer',
+      authorization: [{
+        actor: this.$transit.wallet.auth.accountName,
+        permission: this.$transit.wallet.auth.permission
+      }],
+      data: {
+        from: this.$transit.wallet.auth.accountName,
+        to,
+        quantity: `${parseFloat(quantity).toFixed(process.env.TOKEN_PRECISION)} ${process.env.TOKEN_NAME}`,
+        memo
+      }
+    }]
+  }, {
+    blocksBehind: 3,
+    expireSeconds: 30
+  }]
+
+  console.log(config)
   try {
     const result = await this.$transit.eosApi.transact({
       actions: [{
