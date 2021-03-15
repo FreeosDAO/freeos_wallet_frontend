06e2ef774368c1d651e16f98370fb42aaf7d47b9
wicky <wicky@prideandjoy.org>
2021-03-15 15:03:23 +1300

    add register user button

--- src/components/stake/Stake.vue
+++ src/components/stake/Stake.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    {{claimInfo.respFreeosRecord}}
     <q-card>
       <q-card-section>
         <div class="text-h5 q-pa-md q-mx-auto" style="width: 70%;">
-          Stake {{claimInfo.liquidInAccount.balance}}
+          Stake {{userStakeRequirement}}
         </div>
         <p>To be eligible to Claim your weekly FREEOS tokens</p>
         <div class="q-ma-md q-mt-lg">
@@ -12,7 +11,7 @@
         </div>
       </q-card-section>
     </q-card>
-    <p class="text-center q-mt-md">Current Staking Requirement = {{claimInfo.respStakeRequirement.default_stake}}</p>
+    <p class="text-center q-mt-md">Current Staking Requirement = {{userStakeRequirement}}</p>
   </div>
 </template>
 <script>
@@ -22,19 +21,45 @@ import { getAbsoluteAmount } from '@/utils/currency'
 export default {
   computed: {
     ...mapState({
-      accountName: state => state.account.accountName
+      accountName: state => state.account.accountName,
+      stakedInfo: state => state.account.claimInfo.stakedInfo,
+      respStakeRequirement: state => state.account.claimInfo.respStakeRequirement
     }),
-    ...mapGetters('account', ['claimInfo'])
+    ...mapGetters('account', ['claimInfo']),
+    userStakeRequirement () {
+      if (!this.stakedInfo) {
+        return ''
+      }
+      let accountType = parseInt(this.stakedInfo.account_type, 2)
+      const string = 'abcdefghijklmnopqrstuvwxyz'
+      accountType = string.charAt(accountType - 1)
+      if (!accountType) {
+        return ''
+      }
+      let requirement = null
+      for (const [key, value] of Object.entries(this.respStakeRequirement)) {
+        const requirementType = key.split('_')
+        if (requirementType[1] === accountType) {
+          requirement = value
+          break
+        }
+      }
+      return requirement
+    }
   },
   methods: {
     ...mapActions('stake', ['actionStake']),
     ...mapActions('account', ['getAccountInfo']),
     async onStake () {
-      await this.actionStake(this.accountName)
+      const data = {
+        accountName: this.accountName,
+        amount: this.userStakeRequirement
+      }
+      await this.actionStake(data)
       this.getAccountInfo()
     },
     isMeetStakeMinRequirment () {
-      return getAbsoluteAmount(this.claimInfo.liquidInAccount.balance) >= getAbsoluteAmount(this.claimInfo.respStakeRequirement.default_stake)
+      return getAbsoluteAmount(this.claimInfo.liquidInAccount.balance) >= getAbsoluteAmount(this.userStakeRequirement)
     }
   }
 }
